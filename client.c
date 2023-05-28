47e95f17e85afc02c0e1d4bd2db2294f4db86363
Halim Labouiti <hlabouit@e2r6p4.1337.ma>
2023-05-28 01:52:29 +0100

    tryna get these F*kng bits together and show the char

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 16:26:38 by hlabouit          #+#    #+#             */
-/*   Updated: 2023/05/26 23:48:44 by hlabouit         ###   ########.fr       */
+/*   Updated: 2023/05/28 00:38:06 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 char	*convert_to_binary(char c)
 {
-	int		binary;
-	char	*str_binary;
+	char	*byte;
 	int		reminder;
+	int		binary;
 	int		i;
 
 	binary = 0;
@@ -29,37 +29,37 @@ char	*convert_to_binary(char c)
 		binary = binary + reminder * i;
 		i = i * 10;
 	}
-	str_binary = ft_itoa(binary);
-	return (str_binary);
+	byte = ft_itoa(binary);
+	printf("%s\n", byte);
+	return (byte);
 }
 
 void	client(pid_t pid, char *message)
 {
-	int i;
-	int j;
-	char	*binary;
+	char	*byte;
+	int		i;
+	int		j;
 
 	i = 0;
 	while (message[i])
 	{
 		j = 0;
-		binary = convert_to_binary(message[i]);
-		while (binary[j])
+		byte = convert_to_binary(message[i]);
+		while (byte[j])
 		{
-			if (binary[j] == '0')
+			if (byte[j] == '0')
 				kill(pid, SIGUSR1);
-			usleep(37);
-			if (binary[j] == '1')
+			usleep(1337);
+			if (byte[j] == '1')
 				kill(pid, SIGUSR2);
-			usleep(37);
 			j++;
 		}
-		usleep(37);
+		usleep(1337);
 		i++;
 	}
 }
 
 int main(int ac, char **av)
 {
-	client(14441, "wach les hommes");
+	client(50509, "a");
 }
--- server.c
+++ server.c
@@ -6,25 +6,45 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 19:29:53 by hlabouit          #+#    #+#             */
-/*   Updated: 2023/05/26 23:47:38 by hlabouit         ###   ########.fr       */
+/*   Updated: 2023/05/28 01:46:03 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+#include <math.h>
 
-void type(int sig)
+// void	server()
+
+
+void track_signal(int signal, siginfo_t *info, void *context)
 {
-	if (sig == SIGUSR1)
-		ft_putstr_fd("done\n", 1);
+	char byte[8];
+	static int	i = 0;
+
+	byte[7] = '\0';
+	while (i < 8)
+	{
+		if (signal == SIGUSR1)
+			byte[i] = '0';
+		else if (signal == SIGUSR2)
+			byte[i] = '1';
+		i++;
+	}
+	printf("%s\n", byte);
 }
+
 
 int main(int ac, char **av)
 {
+	printf("%f\n",pow(2, 3));
 	pid_t pid;
 	pid = getpid();
 	ft_putnbr_fd(pid, 1);
 	ft_putstr_fd("\n", 1);
-	signal(SIGUSR1, type);
+	struct sigaction sig_a;
+	sig_a.sa_sigaction = track_signal;
+	sigaction(SIGUSR1, &sig_a, NULL);
+	sigaction(SIGUSR2, &sig_a, NULL);
 	while (1) 
 	{
 		pause();
